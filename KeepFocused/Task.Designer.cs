5c2ca310c2252c380bb09bc1177d08636a03d2ae
Superbil <superbil@gmail.com>
2011-01-13 11:51:14 +0800

    fix Task form Size and button size
    change  txtTask multi line close, font size to 20pt

--- KeepFocused/Task.Designer.cs
+++ KeepFocused/Task.Designer.cs
@@ -42,88 +42,55 @@
             // 
             // txtTask
             // 
-            this.txtTask.AccessibleDescription = null;
-            this.txtTask.AccessibleName = null;
             resources.ApplyResources(this.txtTask, "txtTask");
-            this.txtTask.BackgroundImage = null;
-            this.txtTask.Font = null;
             this.txtTask.Name = "txtTask";
             // 
             // btnOK
             // 
-            this.btnOK.AccessibleDescription = null;
-            this.btnOK.AccessibleName = null;
             resources.ApplyResources(this.btnOK, "btnOK");
-            this.btnOK.BackgroundImage = null;
-            this.btnOK.Font = null;
             this.btnOK.Name = "btnOK";
             this.btnOK.UseVisualStyleBackColor = true;
             this.btnOK.Click += new System.EventHandler(this.butOk_Click);
             // 
             // btnClose
             // 
-            this.btnClose.AccessibleDescription = null;
-            this.btnClose.AccessibleName = null;
-            resources.ApplyResources(this.btnClose, "btnClose");
-            this.btnClose.BackgroundImage = null;
             this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnClose.Font = null;
+            resources.ApplyResources(this.btnClose, "btnClose");
             this.btnClose.Name = "btnClose";
             this.btnClose.UseVisualStyleBackColor = true;
             // 
             // lblWhat
             // 
-            this.lblWhat.AccessibleDescription = null;
-            this.lblWhat.AccessibleName = null;
             resources.ApplyResources(this.lblWhat, "lblWhat");
             this.lblWhat.Name = "lblWhat";
             // 
             // lblSessionDataFile
             // 
-            this.lblSessionDataFile.AccessibleDescription = null;
-            this.lblSessionDataFile.AccessibleName = null;
             resources.ApplyResources(this.lblSessionDataFile, "lblSessionDataFile");
-            this.lblSessionDataFile.Font = null;
             this.lblSessionDataFile.Name = "lblSessionDataFile";
             // 
             // txtSessionDataFileName
             // 
-            this.txtSessionDataFileName.AccessibleDescription = null;
-            this.txtSessionDataFileName.AccessibleName = null;
             resources.ApplyResources(this.txtSessionDataFileName, "txtSessionDataFileName");
-            this.txtSessionDataFileName.BackgroundImage = null;
-            this.txtSessionDataFileName.Font = null;
             this.txtSessionDataFileName.Name = "txtSessionDataFileName";
             this.txtSessionDataFileName.ReadOnly = true;
             // 
             // lblBrowse
             // 
-            this.lblBrowse.AccessibleDescription = null;
-            this.lblBrowse.AccessibleName = null;
             resources.ApplyResources(this.lblBrowse, "lblBrowse");
-            this.lblBrowse.BackgroundImage = null;
-            this.lblBrowse.Font = null;
             this.lblBrowse.Name = "lblBrowse";
             this.lblBrowse.UseVisualStyleBackColor = true;
             // 
             // btnViewDataFile
             // 
-            this.btnViewDataFile.AccessibleDescription = null;
-            this.btnViewDataFile.AccessibleName = null;
             resources.ApplyResources(this.btnViewDataFile, "btnViewDataFile");
-            this.btnViewDataFile.BackgroundImage = null;
-            this.btnViewDataFile.Font = null;
             this.btnViewDataFile.Name = "btnViewDataFile";
             this.btnViewDataFile.UseVisualStyleBackColor = true;
             this.btnViewDataFile.Click += new System.EventHandler(this.btnViewDataFile_Click);
             // 
             // btnChengFilePath
             // 
-            this.btnChengFilePath.AccessibleDescription = null;
-            this.btnChengFilePath.AccessibleName = null;
             resources.ApplyResources(this.btnChengFilePath, "btnChengFilePath");
-            this.btnChengFilePath.BackgroundImage = null;
-            this.btnChengFilePath.Font = null;
             this.btnChengFilePath.Name = "btnChengFilePath";
             this.btnChengFilePath.UseVisualStyleBackColor = true;
             this.btnChengFilePath.Click += new System.EventHandler(this.btnChengFilePath_Click);
@@ -131,11 +98,8 @@
             // Task
             // 
             this.AcceptButton = this.btnOK;
-            this.AccessibleDescription = null;
-            this.AccessibleName = null;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = null;
             this.CancelButton = this.btnClose;
             this.Controls.Add(this.btnChengFilePath);
             this.Controls.Add(this.btnViewDataFile);
@@ -146,7 +110,6 @@
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.txtTask);
-            this.Font = null;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Task";
             this.Load += new System.EventHandler(this.Task_Load);
